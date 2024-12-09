b1bdb3f4ab869f18ea3c30c3fc150d4c873fce85
MartinFerret <mferret.pro@gmail.com>
2024-12-09 10:14:20 +0100

    fix(): cors updated globally.

--- back/src/main/java/com/example/back/controller/SkillController.java
+++ back/src/main/java/com/example/back/controller/SkillController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/skills")
-@CrossOrigin(origins = "http://localhost:4200")
 public class SkillController {
     private final SkillService skillService;
 
